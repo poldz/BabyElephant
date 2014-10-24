88dd6759211c0e673c11cf99bc4469d3cad5f7a0
poldz <leopoldo.agdeppa@gmail.com>
2014-10-24 11:22:42 +0800

    re adding routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,58 +1,43 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
 
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
+  # get "home/index"
+  devise_for :users
+  root :to => "home#index"
 
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
 
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
 
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
 
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
+  resources :accounts do
+    resources :accounting_periods do
 
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', :on => :collection
-  #     end
-  #   end
+      member do
+        get 'generate_report'
+        get 'forecast'
+        get 'summary'
+      end
 
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+      resources :receipts
+      resources :bank_deposits
 
-  # See how all your routes lay out with "rake routes"
+      resources :bank_withdrawals  do
+        resources :remittances
+        resources :expenses
+      end
 
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
+      resources :bank_interests
+      resources :bank_taxes
+      resources :bank_penalties
+      resources :bank_fees
+      resources :bank_other_transactions
+      resources :remittances
+      resources :expenses
+
+
+    end
+  end
+
+
+
+
 end
